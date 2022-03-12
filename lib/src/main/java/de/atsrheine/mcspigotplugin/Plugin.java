960f62f5b14d0e6347d3a2f14684c7b80c08d51d
Noah Albers <albers-noah@pm.me>
2022-03-12 20:39:14 +0100

    Moved project to gradle-dependency-manager and therefor did some file-refactoring

--- lib/src/main/java/de/atsrheine/mcspigotplugin/Plugin.java
+++ lib/src/main/java/de/atsrheine/mcspigotplugin/Plugin.java
@@ -2,10 +2,17 @@ package de.atsrheine.mcspigotplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import de.atsrheine.mcspigotplugin.discord.DiscordConnector;
+import gitignore.DEBUG_CONSTANTS;
+
 public class Plugin extends JavaPlugin{
 	
 	// Prefix for the plugin
 	public static final String PREFIX = "§8[§cA§aT§bS§7-§7Bot§8]";
+	
+	// Global discord connection
+	// TODO: Load dc-token from file
+	public static final DiscordConnector DC_CON = new DiscordConnector(DEBUG_CONSTANTS.DC_TOKEN);
 	
 	@Override
 	public void onEnable() {

bccadc149668e0a02736a826686d4c4454b41010
Noah Albers <albers-noah@pm.me>
2022-03-12 16:38:24 +0100

    Kicked off general Project creation with adding all dependencys and the basic project-package structure.

---
+++ src/de/atsrheine/mcspigotplugin/Plugin.java
@@ -0,0 +1,14 @@
+package de.atsrheine.mcspigotplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin{
+	
+	// Prefix for the plugin
+	public static final String PREFIX = "§8[§cA§aT§bS§7-§7Bot§8]";
+	
+	@Override
+	public void onEnable() {
+		System.out.println("Yooooo läuft");
+	}
+}
